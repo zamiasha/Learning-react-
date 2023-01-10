f98817cbed0e4626d664b482d6bd5fea593c6b10
zamia s <sherzaman@geniusmindzone.com>
2023-01-10 15:21:54 +0500

    functional states  and  create  button update the data on click with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import User from './components/User';
 
 function App() {
-  
+
+    const [data, setData]=useState("ali")
+    function updateData() {
+        setData("zaman")
+    }
+    console.warn("__________________");
     return ( 
         <div className="App">
-        <h1> sher </h1>
-        <button>Update Date</button>
+        <h1>{data} </h1>
+        <button onClick={updateData}>Update Date</button>
         <User />
        
        </div>
