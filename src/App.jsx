0db906125380f7e63d465af0927038bcfd96cc4a
zamia s <sherzaman@geniusmindzone.com>
2023-01-10 15:58:13 +0500

    on click button update  data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import User from './components/User';
+// import User from './components/User';
 
 function App() {
 
-    const [data, setData]=useState("ali")
+    const [data, setData]=useState(0)
     function updateData() {
-        setData("zaman")
+        setData(data + 1)
     }
     console.warn("__________________");
     return ( 
         <div className="App">
         <h1>{data} </h1>
         <button onClick={updateData}>Update Date</button>
-        <User />
+        
        
        </div>
     );
