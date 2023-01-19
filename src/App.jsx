32be6b929900ef04058af8f1701994534dfc9eb1
zamia s <sherzaman@geniusmindzone.com>
2023-01-19 12:53:53 +0500

    Construcure life cycle methode by Class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,47 @@
-import React,{useState} from 'react';
+import React from 'react';
+// import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import Uses from './components/Uses'
-import MemberUses from './components/MemberUses'
+// import Login from './components/Login';
+// import Uses from './components/Uses'
+// import MemberUses from './components/MemberUses'
+import Constructtrlifecycle from './components/Constructtrlifecycle';
 
 
 function App() { 
     
 
-     const [list, setList] = useState([]);
-     const [input, setInput] = useState("");
-
-
-     const addTodo = (todo) => {
-         const newTodo = {
-            id: Math.random(),
-            todo: todo
-         }
-
-         //add todo  to  the  list
-         setList([...list, newTodo])
-
-
-         //clear input  box
-         setInput("");
-
-
-         
-         }
-         const deleteTodo = (id) => {
-            //filter out the todo with id  
-
-            const  newList = list.filter((todo) => todo.id !== id);
-
-
-            setList(newList);
-     }
-
     return (
         <div className="App">         
               <h1>Listing</h1>
 
-              <input type="text" value={input} onChange={(e) =>setInput(e.target.value)} />
-              <button onClick={() => addTodo(input)}>Add</button>
-              <ul className='liui'>
-                {list.map((todo) => (
-                    <li key={todo.id}>
-                    {todo.todo}
-                     <button onClick={() => deleteTodo(todo.id)}>&times;</button>
-                    </li>
-                ))}
-              </ul>
+            <Constructtrlifecycle />
+
+              
         </div>
     )
-}
-    
-    //    function getData() {
-    //     alert("hellow from app")
-    //    }
-    // return ( 
-        // <div className="App">
-        // <h1>Pass Function as Props ) 😯</h1>
 
-        // <Uses data={getData} />
+// class App extends React.Component{
 
-        // <div className="Member-Uses">
-        // <MemberUses data={getData} />
-        // </div>
-        
-  
       
-//        </div>
-//     );
+//     Constructor () {
+//        this.State={
+//         data:"zaman"
 
-// }
+//        }
+//     }
+
+//     render () {
+      
+
+//         return (
+//             <div>
+//                 <h1>hello i am here{this.State.data} </h1>
+//             </div>
+//         )
+//     }
+// }}
+    }
 
 
 
