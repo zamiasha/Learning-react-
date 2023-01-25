a4f42adc892b78ede835aeb62f58d3beb9b4600c
zamia s <sherzaman@geniusmindzone.com>
2023-01-25 10:41:29 +0500

    hooks  used  for  class components  features in functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,25 @@
-// import React, {useState} from 'react';
-import React from 'react';
-// import React,{useState} from 'react';
+// import React from 'react';
+import React, {useState, use} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import Login from './components/Login';
-// import Uses from './components/Uses'
-// import MemberUses from './components/MemberUses'
-// import Constructtrlifecycle from './components/Constructtrlifecycle';
-
-// import DidMountlifecycle from './components/DidMountlifecycle'
 
 
 
 
-// testing and  impletation  Comment 
-// function App() { 
+
+
+function App() { 
          
-//     return (
-//         <div className="App">         
-//               <h1>Lifecycle Methode DidMountlifecycle</h1>
-              
-//         </div>
-//     )
+    const [data, setData] = useState("zaman");
+    return (
+        <div className="App">         
+              <h1>{data}</h1>
+        
+              <button onClick={() => setData("sher")}>update data </button>
+        </div>
+    )
 
-// }
-
-
-
- class  App extends React.Component {
-
-    constructor () {
-        super();
-         this.state={
-            name:"ali"
-         }
-    } 
-    componentDidMount () {
-        console.warn("all over  them s")
-    }
-    render () {
-        console.warn("lets finsih ")
-        return (
-            <div className='App'>
-                <h1 style={{color: 'red'}}>
-                     life cycle => didmount methode {this.state.name} 
-                </h1>
-                <button onClick={() => {this.setState({name:"zamnan"})}} >Update  state content</button>
-            </div>
-        )
-    }
 }
 
 
