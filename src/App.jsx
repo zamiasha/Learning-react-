c08b0b43157df6c07d10415f67a5f7d90f724762
zamia s <sherzaman@geniusmindzone.com>
2023-01-31 16:50:19 +0500

    import bootstrap pakg  and  added  design shapes here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import React, { useEffect, useState, usestate} from 'react';
 import React from 'react';
-import "./custom.css"
+import "./custom.css";
 import style from './Custom.module.css'
 // import logo from './logo.svg';
 import './App.css';
-import Specificdata from './components/Specificdata';
+// import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import De from './components/De'
+
 // import Login from './components/Login';
 
 
@@ -18,6 +21,22 @@ console.log("style???", style);
        <h1 className='primary'>Style 1  user here </h1>
        <h1 style={{color: 'red',backgroundColor:  'yellow' }}>Style 2  user here </h1>
        <h1 className={style.success}>Style 3  user here </h1>
+
+       <Button onClick={()=> alert("button work ")}>Hello Testing user here</Button>
+       <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+
+       <De />
+      
+
+       
 
 
         </div>
