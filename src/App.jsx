c4613a87915fa213f5f0a6b1a399097ab0d53587
zamia s <sherzaman@geniusmindzone.com>
2023-02-01 12:08:45 +0500

    use map funciton and for loop without  return

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,34 @@ import style from './Custom.module.css'
 // import logo from './logo.svg';
 import './App.css';
 // import { Button } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import De from './components/De'
-
+// import Button from 'react-bootstrap/Button';
+// import De from './components/De'
 // import Login from './components/Login';
 
 
 
 function App() { 
-    
-console.log("style???", style);
+      
+   const student = ['user1', 'user2', 'user3', 'zahib'];
+
+  student.map((item)=>{
+    console.log("my name is : ", item)
+  })
+  for (let i=0; i<student.length; i++) {
+    console.warn("in for loop my name is :", student[i]) 
+  }
     return (
         <div className="App"> 
+        <h3>User testing </h3>
+        {/* {
+          student.map((data)=>
+            <h1>Name is : {data}</h1>
+          )
+        } */}
      
-       <h1 className='primary'>Style 1  user here </h1>
-       <h1 style={{color: 'red',backgroundColor:  'yellow' }}>Style 2  user here </h1>
-       <h1 className={style.success}>Style 3  user here </h1>
-
-       <Button onClick={()=> alert("button work ")}>Hello Testing user here</Button>
-       <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-
-       <De />
-      
-
-       
-
-
+    
         </div>
     )
 }
-
-
-   
-
 
 export default App;
